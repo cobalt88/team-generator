380059d7cd73a3879ea440e1772630dcbf65f87b
cobalt88 <vincentteune@gmail.com>
2022-04-27 05:48:09 -0400

    4.27.22
    got the classes working properly with objects being passed into them instead of parameters, worote out test functions.

--- _test_/Intern.test.js
+++ _test_/Intern.test.js
@@ -1,1 +1,14 @@
 //intern test needs to add test for school on top of normal employee test
+//intern test needs to test for office number on top of normal employee test
+const { TestWatcher } = require("jest");
+const Employee = require("../lib/Employee");
+const Intern =  require("../lib/Intern");
+
+test('creates intern class', () => {
+  const intern = new intern({name: 'sallie', id: 1234, email: 'intern@bobsburers.com', school: UCF});
+
+  expect(intern.name).toEqual(expect.any(String));
+  expect(intern.id).toEqual(expect.any(Number));
+  expect(intern.email).toEqual(expect.any(String));
+  expect(intern.school).toEqual(expect.any(Number));
+})
